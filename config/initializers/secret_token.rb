5e985fcdaf9a3530dcc5c78bf4b0793e5c673f6e
laihui0207 <laihui0207gm@gmail.com>
2011-10-26 23:46:26 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunnotes::Application.config.secret_token = '8ba91b0f3cbcc5f789e3b89880a620548514da488ad990115d332361c7d3ffaa1ef5549282062a06d096f359c84d1924631bb5296c2e692538350b1832688973'
